ecf39e8a72099f4a22ae94c4af72562bcff2f4e4
AlwaysBeHigh <titenfool@mail.ru>
2024-07-03 20:33:46 +0300

    1

--- shop/nuxt.config.ts
+++ shop/nuxt.config.ts
@@ -1,27 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    devtools: {enabled: true},
-    app: {
+  ssr: false,
+  devtools: {enabled: true},
 
-        head: {
-            link: [
-                {rel: 'stylesheet', href: 'https://unpkg.com/primeflex@latest/primeflex.css'},
-                {rel: 'stylesheet', href: 'https://unpkg.com/primevue/resources/themes/lara-light-indigo/theme.css'}
-            ],
-            title: 'Интернет магазин',
-            meta: [
-                { charset: 'utf-8' },
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            ],
+  app: {
 
-        },
+      head: {
+          link: [
+              {rel: 'stylesheet', href: 'https://unpkg.com/primeflex@latest/primeflex.css'},
+              {rel: 'stylesheet', href: 'https://unpkg.com/primevue/resources/themes/lara-light-indigo/theme.css'}
+          ],
+          title: 'Интернет магазин',
+          meta: [
+              { charset: 'utf-8' },
+              { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          ],
 
-        pageTransition: {name: 'page', mode: 'out-in'},
-        spaLoadingTemplate: true,
-    },
-    modules: [
-        '@pinia/nuxt',
-        '@pinia-plugin-persistedstate/nuxt',
-    ],
+      },
+
+      pageTransition: {name: 'page', mode: 'out-in'},
+      spaLoadingTemplate: true,
+  },
+
+  modules: [
+      '@pinia/nuxt',
+      '@pinia-plugin-persistedstate/nuxt',
+  ],
+
+  compatibilityDate: '2024-07-03',
 })
